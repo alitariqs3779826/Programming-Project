c844cb8492af9a31c33f10067e06abce7f71daf0
[Prabhav Mehra s3743761] <s3743761@student.rmit.edu.au>
2021-03-08 19:29:57 +1100

    Create table and redirecting to create post form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {MyAppBar} from './Components/AppBar';
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import Hire from './Components/Hire';
-
+import CreatePost from './Components/CreatePost';
 
 // class App extends Component{
   
@@ -26,7 +26,8 @@ function App() {
         <BrowserRouter>
        
            <Switch>
-           <Route path="/hire" component={Hire}></Route>       
+           <Route path="/hire" component={Hire}></Route>      
+           <Route path="/createPost" component={CreatePost}></Route>       
                   
             </Switch>
         </BrowserRouter> 
